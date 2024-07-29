71920704e650fbce8e222c51fdce18e269674abf
Sharon Lee <sharonal@stanford.edu>
2024-07-29 14:54:07 -0700

    new p2p

--- p2p.sh
+++ p2p.sh
@@ -1,18 +1,18 @@
 ### 1st step: get the background mask and inversion files for DPL
 IMG_FORMAT='jpg'
 IMG_FOLDER='images/'
-FILE_NAME='catdog'
+FILE_NAME='rendered_chair'
 
 # CUDA_VISIBLE_DEVICES=1 python _2_DDIM_inv.py \
 #     --input_image ${IMG_FOLDER}/${FILE_NAME}.${IMG_FORMAT} \
 #     --results_folder output/ \
 
 ### 2nd step: Dynamic Prompt Learning
-PLACEHOLDER1='<cat-toy>'
-PLACEHOLDER2='<dog-toy>'
+PLACEHOLDER1='<chair>'
+# PLACEHOLDER2='<dog-toy>'
 
-INIT_TOKEN1='cat'
-INIT_TOKEN2='dog'
+INIT_TOKEN1='chair'
+# INIT_TOKEN2='dog'
 
 MAX_ITER=0
 
@@ -40,6 +40,6 @@ python _4_image_edit.py \
 --results_folder output/${FILE_NAME}/ \
 --results_folder_edit output/test \
 --negative_guidance_scale 7.5 \
---placeholder_token ${PLACEHOLDER1} ${PLACEHOLDER2} \
---initializer_token ${INIT_TOKEN1} ${INIT_TOKEN2} \
---replaced_embed_folder output/catdog_old2
+--placeholder_token ${PLACEHOLDER1} \
+--initializer_token ${INIT_TOKEN1} \
+--replaced_embed_folder output/rendered_chair_5
