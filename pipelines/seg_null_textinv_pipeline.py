f33f724d2b43523e646ee80cbec173244ed03d1b
Sharon Lee <sharonal@stanford.edu>
2024-07-29 02:49:48 -0700

    test out half img

--- pipelines/seg_null_textinv_pipeline.py
+++ pipelines/seg_null_textinv_pipeline.py
@@ -1046,12 +1046,12 @@ class StableDiffusion_SegPipeline(DiffusionPipeline):
                         curr_image = curr_image.cpu().permute(0, 2, 3, 1).float().detach().numpy()[0]
                         from PIL import Image
                         import pdb; pdb.set_trace()
-                        curr_image_save = Image.fromarray(curr_image)
+                        curr_image_save = Image.fromarray((curr_image * 255).astype(np.uint8))
                         # Save the image as a JPEG file
                         curr_image_save.save('whole_img.jpg')
                         half_img = curr_image
                         half_img[:, half_img.shape[1] // 2:, :] = 0
-                        half_img_save = Image.fromarray(half_img)
+                        half_img_save = Image.fromarray((half_img * 255).astype(np.uint8))
                         half_img_save.save('half_img.jpg')
 
                         half_img_enc = torch.tensor(half_img).permute(2, 0, 1).unsqueeze(0).to(latent_model_input.device)
@@ -1062,7 +1062,7 @@ class StableDiffusion_SegPipeline(DiffusionPipeline):
                         half_img_enc = (half_img_enc / 2 + 0.5).clamp(0, 1)
                         # we always cast to float32 as this does not cause significant overhead and is compatible with bfloat16
                         half_img_enc = half_img_enc.cpu().permute(0, 2, 3, 1).float().numpy()[0]
-                        half_img_enc = Image.fromarray(half_img_enc)
+                        half_img_enc = Image.fromarray((half_img_enc * 255).astype(np.uint8))
                         # Save the image as a JPEG file
                         half_img_enc.save('half_img_enc.jpg')
 
