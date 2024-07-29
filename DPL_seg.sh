b5fc8aa907af717b1fbc04bf75ec710e71d04f8e
Sharon Lee <sharonal@stanford.edu>
2024-07-28 21:23:56 -0700

    script fix

--- DPL_seg.sh
+++ DPL_seg.sh
@@ -31,5 +31,4 @@ CUDA_VISIBLE_DEVICES=0 python _3_dpl_seg_inv.py \
     --lam_kl  ${lam_kl} \
     --smooth_op \
     --softmax_op \
-    --seg_dirs seg_dirs/${FILE_NAME} \
-    --num_ddim_steps 5
+    --seg_dirs seg_dirs/${FILE_NAME} 
