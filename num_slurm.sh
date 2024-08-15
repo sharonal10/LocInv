32a85bc6d61cc12222816ac45f7ef3502c488884
Sharon Lee <sharonal@stanford.edu>
2024-08-15 15:11:22 -0700

    try alt

--- num_slurm.sh
+++ num_slurm.sh
@@ -94,8 +94,8 @@ vals=(10 20 30 40 50)
 for val in "${vals[@]}"; do
   python -m tu.sbatch.sbatch_sweep --time 96:00:00 \
   --proj_dir /viscam/projects/image2Blender/differentiable_engine/LocInv --conda_env dpl \
-  --job "08-15-${val}" --command "rm -r output/08-15-early-stop-${val} && cp -r output/08-15-early-stop output/08-15-early-stop-${val}  && python _3_dpl_seg_inv.py --input_image images/chair1.jpg --results_folder output/08-15-early-stop-${val} --initializer_token chair --placeholder_token  '<chair>' --smooth_op --softmax_op --seg_dirs seg_dirs/rendered_chair --exp_name 08-15-early-stop-${val} --early_stop ${val}" $GPU_INFO
+  --job "08-15-${val}" --command "rm -r output/08-15-early-stop-${val}; cp -r output/08-15-early-stop output/08-15-early-stop-${val}  && python _3_dpl_seg_inv.py --input_image images/chair1.jpg --results_folder output/08-15-early-stop-${val} --initializer_token chair --placeholder_token  '<chair>' --smooth_op --softmax_op --seg_dirs seg_dirs/rendered_chair --exp_name 08-15-early-stop-${val} --early_stop ${val}" $GPU_INFO
 done
 
 
-rm -r output/08-15-early-stop-1 && cp -r output/08-15-early-stop output/08-15-early-stop-1 && python _3_dpl_seg_inv.py --input_image images/chair1.jpg --results_folder output/08-15-early-stop-1 --initializer_token chair --placeholder_token  '<chair>' --smooth_op --softmax_op --seg_dirs seg_dirs/rendered_chair --exp_name 08-15-early-stop-1 --early_stop 1
+# rm -r output/08-15-early-stop-1 && cp -r output/08-15-early-stop output/08-15-early-stop-1 && python _3_dpl_seg_inv.py --input_image images/chair1.jpg --results_folder output/08-15-early-stop-1 --initializer_token chair --placeholder_token  '<chair>' --smooth_op --softmax_op --seg_dirs seg_dirs/rendered_chair --exp_name 08-15-early-stop-1 --early_stop 1
