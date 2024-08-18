5b41ea7f3df4a8dc2f8dac609a88db1dd0c90161
Sharon Lee <sharonal@stanford.edu>
2024-08-17 20:42:44 -0700

    try wth diff betas?

--- num_slurm.sh
+++ num_slurm.sh
@@ -60,9 +60,13 @@ EXTRA_GPU_INFO="exclude=viscam1,viscam5,viscam7,svl[1-6],svl[8-10]"
 
 vals=(0.0 0.3 0.6 0.9 1.2)
 for val in "${vals[@]}"; do
+  # python -m tu.sbatch.sbatch_sweep --time 96:00:00 \
+  # --proj_dir /viscam/projects/image2Blender/differentiable_engine/LocInv --conda_env dpl \
+  # --job "08-16-black_chair-${val}" --command "python _2_DDIM_inv.py --input_image images/chair_black.jpg --prompt_str 'a photo of a chair' --results_folder ./output/08-16-black_chair-${val} && python _3_dpl_seg_inv.py --input_image images/chair_black.jpg --results_folder output/08-16-black_chair-${val} --initializer_token chair --placeholder_token  '<chair-real>' --smooth_op --softmax_op --seg_dirs seg_dirs/black_chair --exp_name 08-16-black_chair-${val}  --beta_cos ${val} --beta_iou ${val}" $GPU_INFO "$EXTRA_GPU_INFO"
+  
   python -m tu.sbatch.sbatch_sweep --time 96:00:00 \
   --proj_dir /viscam/projects/image2Blender/differentiable_engine/LocInv --conda_env dpl \
-  --job "08-16-black_chair-${val}" --command "python _2_DDIM_inv.py --input_image images/chair_black.jpg --prompt_str 'a photo of a chair' --results_folder ./output/08-16-black_chair-${val} && python _3_dpl_seg_inv.py --input_image images/chair_black.jpg --results_folder output/08-16-black_chair-${val} --initializer_token chair --placeholder_token  '<chair-real>' --smooth_op --softmax_op --seg_dirs seg_dirs/black_chair --exp_name 08-16-black_chair-${val}  --beta_cos ${val} --beta_iou ${val} --beta_kl ${val} --beta_sim ${val} --beta_adj 0.0" $GPU_INFO "$EXTRA_GPU_INFO"
+  --job "08-16-black_chair-${val}" --command "python _3_dpl_seg_inv.py --input_image images/chair_black.jpg --results_folder output/08-16-black_chair-${val} --initializer_token chair --placeholder_token  '<chair-real>' --smooth_op --softmax_op --seg_dirs seg_dirs/black_chair --exp_name 08-16-black_chair-${val}  --beta_cos ${val} --beta_iou ${val}" $GPU_INFO "$EXTRA_GPU_INFO"
 done
 
 
