d6ff02f0fbc593fd06642ce92eef9004757ecfa2
Sharon Lee <sharonal@stanford.edu>
2024-08-19 09:56:41 -0700

    p3 again

--- num_slurm.sh
+++ num_slurm.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-GPU_INFO="--partition viscam --account viscam --gpu_type 3090 --cpus_per_task 8 --num_gpus 1 --mem 100G"
-# GPU_INFO="--partition viscam --account viscam --gpu_type a6000 --cpus_per_task 8 --num_gpus 1 --mem 24G"
+# GPU_INFO="--partition viscam --account viscam --gpu_type 3090 --cpus_per_task 8 --num_gpus 1 --mem 100G"
+GPU_INFO="--partition viscam --account viscam --gpu_type a6000 --cpus_per_task 8 --num_gpus 1 --mem 24G"
 # GPU_INFO="--partition viscam --account viscam --gpu_type titanrtx --cpus_per_task 8 --num_gpus 1 --mem 64G"
 
 # GPU_INFO="--partition svl --account viscam --gpu_type titanrtx --cpus_per_task 8 --num_gpus 1 --mem 64G"
 
-EXTRA_GPU_INFO="exclude=viscam1,viscam5,viscam7,svl[1-6],svl[8-10]"
+# EXTRA_GPU_INFO="exclude=viscam1,viscam5,viscam7,svl[1-6],svl[8-10]"
+EXTRA_GPU_INFO=""
 
 
 # python -m tu.sbatch.sbatch_sweep --time 96:00:00 \
